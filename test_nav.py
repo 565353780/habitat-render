ddb8a1f97ceac890a84c9f9b74b8a002dfc2c8c7
chLi <565353780@qq.com>
2022-06-05 01:01:39 +0800

    add custom cfg creater code

--- test_nav.py
+++ test_nav.py
@@ -8,11 +8,84 @@ from habitat_sim.utils.common import quat_from_angle_axis, d3_40_colors_rgb
 from PIL import Image
 from matplotlib import pyplot as plt
 
+def make_cfg(settings):
+    sim_cfg = habitat_sim.SimulatorConfiguration()
+    sim_cfg.gpu_device_id = 0
+    sim_cfg.scene_id = settings["scene"]
+    sim_cfg.enable_physics = settings["enable_physics"]
+
+    sensor_specs = []
+
+    color_sensor_spec = habitat_sim.CameraSensorSpec()
+    color_sensor_spec.uuid = "color_sensor"
+    color_sensor_spec.sensor_type = habitat_sim.SensorType.COLOR
+    color_sensor_spec.resolution = [settings["height"], settings["width"]]
+    color_sensor_spec.position = [0.0, settings["sensor_height"], 0.0]
+    color_sensor_spec.sensor_subtype = habitat_sim.SensorSubType.PINHOLE
+    sensor_specs.append(color_sensor_spec)
+
+    depth_sensor_spec = habitat_sim.CameraSensorSpec()
+    depth_sensor_spec.uuid = "depth_sensor"
+    depth_sensor_spec.sensor_type = habitat_sim.SensorType.DEPTH
+    depth_sensor_spec.resolution = [settings["height"], settings["width"]]
+    depth_sensor_spec.position = [0.0, settings["sensor_height"], 0.0]
+    depth_sensor_spec.sensor_subtype = habitat_sim.SensorSubType.PINHOLE
+    sensor_specs.append(depth_sensor_spec)
+
+    semantic_sensor_spec = habitat_sim.CameraSensorSpec()
+    semantic_sensor_spec.uuid = "semantic_sensor"
+    semantic_sensor_spec.sensor_type = habitat_sim.SensorType.SEMANTIC
+    semantic_sensor_spec.resolution = [settings["height"], settings["width"]]
+    semantic_sensor_spec.position = [0.0, settings["sensor_height"], 0.0]
+    semantic_sensor_spec.sensor_subtype = habitat_sim.SensorSubType.PINHOLE
+    sensor_specs.append(semantic_sensor_spec)
+
+    agent_cfg = habitat_sim.agent.AgentConfiguration()
+    agent_cfg.sensor_specifications = sensor_specs
+    agent_cfg.action_space = {
+        "move_forward": habitat_sim.agent.ActionSpec(
+            "move_forward", habitat_sim.agent.ActuationSpec(amount=0.25)
+        ),
+        "turn_left": habitat_sim.agent.ActionSpec(
+            "turn_left", habitat_sim.agent.ActuationSpec(amount=30.0)
+        ),
+        "turn_right": habitat_sim.agent.ActionSpec(
+            "turn_right", habitat_sim.agent.ActuationSpec(amount=30.0)
+        ),
+    }
+    return habitat_sim.Configuration(sim_cfg, [agent_cfg])
+
+def print_scene_recur(scene, limit_output=10):
+    print(
+        f"House has {len(scene.levels)} levels, {len(scene.regions)} regions and {len(scene.objects)} objects"
+    )
+    print(f"House center:{scene.aabb.center} dims:{scene.aabb.sizes}")
+
+    count = 0
+    for level in scene.levels:
+        print(
+            f"Level id:{level.id}, center:{level.aabb.center},"
+            f" dims:{level.aabb.sizes}"
+        )
+        for region in level.regions:
+            print(
+                f"Region id:{region.id}, category:{region.category.name()},"
+                f" center:{region.aabb.center}, dims:{region.aabb.sizes}"
+            )
+            for obj in region.objects:
+                print(
+                    f"Object id:{obj.id}, category:{obj.category.name()},"
+                    f" center:{obj.aabb.center}, dims:{obj.aabb.sizes}"
+                )
+                count += 1
+                if count >= limit_output:
+                    return
+    return True
+
 def display_sample(rgb_obs=None, semantic_obs=None, depth_obs=None):
     if rgb_obs is None and semantic_obs is None and depth_obs is None:
         return True
 
-    rgb_img = None
     semantic_img = None
     depth_img = None
 
@@ -20,7 +93,6 @@ def display_sample(rgb_obs=None, semantic_obs=None, depth_obs=None):
     titles = []
 
     if rgb_obs is not None:
-        rgb_img = Image.fromarray(rgb_obs, mode="RGB")
         arr.append(rgb_obs)
         titles.append('rgb')
 
@@ -46,32 +118,26 @@ def display_sample(rgb_obs=None, semantic_obs=None, depth_obs=None):
     plt.show()
     return True
 
-def make_simple_cfg(settings):
-    sim_cfg = habitat_sim.SimulatorConfiguration()
-    sim_cfg.scene_id = settings["scene"]
-
-    rgb_sensor_spec = habitat_sim.CameraSensorSpec()
-    rgb_sensor_spec.uuid = "color_sensor"
-    rgb_sensor_spec.sensor_type = habitat_sim.SensorType.COLOR
-    rgb_sensor_spec.resolution = [settings["height"], settings["width"]]
-    rgb_sensor_spec.position = [0.0, settings["sensor_height"], 0.0]
-
-    agent_cfg = habitat_sim.agent.AgentConfiguration()
-    agent_cfg.sensor_specifications = [rgb_sensor_spec]
-
-    return habitat_sim.Configuration(sim_cfg, [agent_cfg])
-
 test_scene = "/home/chli/habitat/scannet/scans/scene0474_02/scene0474_02_vh_clean.glb"
 
 sim_settings = {
+    "width": 256,
+    "height": 256,
     "scene": test_scene,
     "default_agent": 0,
     "sensor_height": 1.5,
-    "width": 256,
-    "height": 256}
-cfg = make_simple_cfg(sim_settings)
+    "color_sensor": True,
+    "depth_sensor": True,
+    "semantic_sensor": False,
+    "seed": 1,
+    "enable_physics": False,
+}
+cfg = make_cfg(sim_settings)
 
 sim = habitat_sim.Simulator(cfg)
+
+scene = sim.semantic_scene
+print_scene_recur(scene)
 
 agent = sim.initialize_agent(sim_settings["default_agent"])
 
@@ -92,7 +158,8 @@ def navigateAndSee(action=""):
         observations = sim.step(action)
         print("action: ", action)
         if display:
-            display_sample(observations["color_sensor"])
+            display_sample(rgb_obs=observations["color_sensor"],
+                           depth_obs=observations["depth_sensor"])
 
 for _ in range(100):
     action = action_names[randint(0, len(action_names) - 1)]
