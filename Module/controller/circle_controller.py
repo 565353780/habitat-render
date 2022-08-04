8544135ad86b1cb9ad772910806a5edfd075936c
chLi <565353780@qq.com>
2022-08-04 16:29:15 +0800

    finish basic circle controller

--- Module/controller/circle_controller.py
+++ Module/controller/circle_controller.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from Data.point import Point
-
+from Config.config import SIM_SETTING
 from Config.init_pose import INIT_RADIUS
 from Config.input_map import INPUT_KEY_DICT
-
-from Method.rotations import getDirectionFromRad
 
 from Method.poses import \
     getMoveForwardPose, getMoveLeftPose, \
     getMoveRightPose, getMoveBackwardPose, \
-    getMoveUpPose, getMoveDownPose, \
-    getTurnLeftPose, getTurnRightPose, \
-    getTurnUpPose, getTurnDownPose, \
-    getHeadLeftPose, getHeadRightPose
+    getMoveUpPose, getMoveDownPose
+from Method.circle_poses import \
+    getCircleTurnLeftPose, getCircleTurnRightPose, \
+    getCircleTurnUpPose, getCircleTurnDownPose, \
+    getCircleHeadLeftPose, getCircleHeadRightPose
 
 from Module.controller.base_pose_controller import BasePoseController
 
@@ -29,6 +27,9 @@ class CircleController(BasePoseController):
         super().reset()
         self.center_pose = self.pose
         self.radius = INIT_RADIUS
+        return True
+
+    def updateCenterPose(self):
         return True
 
     def updatePose(self):
@@ -59,44 +60,44 @@ class CircleController(BasePoseController):
         return True
 
     def turnLeft(self, rotate_angle):
-        self.pose = getTurnLeftPose(self.pose, rotate_angle)
+        self.pose = getCircleTurnLeftPose(self.pose, self.radius, rotate_angle)
         return True
 
     def turnRight(self, rotate_angle):
-        self.pose = getTurnRightPose(self.pose, rotate_angle)
+        self.pose = getCircleTurnRightPose(self.pose, self.radius, rotate_angle)
         return True
 
     def turnUp(self, rotate_angle):
-        self.pose = getTurnUpPose(self.pose, rotate_angle)
+        self.pose = getCircleTurnUpPose(self.pose, self.radius, rotate_angle)
         return True
 
     def turnDown(self, rotate_angle):
-        self.pose = getTurnDownPose(self.pose, rotate_angle)
+        self.pose = getCircleTurnDownPose(self.pose, self.radius, rotate_angle)
         return True
 
     def headLeft(self, rotate_angle):
-        self.pose = getHeadLeftPose(self.pose, rotate_angle)
+        self.pose = getCircleHeadLeftPose(self.pose, self.radius, rotate_angle)
         return True
 
     def headRight(self, rotate_angle):
-        self.pose = getHeadRightPose(self.pose, rotate_angle)
+        self.pose = getCircleHeadRightPose(self.pose, self.radius, rotate_angle)
+        return True
+
+    def test(self):
+        super().test()
+
+        input_key_list = INPUT_KEY_DICT.keys()
+        for input_key in input_key_list:
+            agent_state = self.getAgentStateByKey(input_key)
+            print("[INFO][CircleController::test]")
+            print("\t getAgentStateByKey")
+            print("\t agent_state: position", agent_state.position,
+                  "rotation", agent_state.rotation)
         return True
 
 def demo():
-    position = Point(2.7, 1.5, -3.0)
-    look_at = Point(1.0, 0.5, -5.5)
-    move_direction = Point(1.0, 1.0, 3.0)
-
     circle_controller = CircleController()
 
-    pose = circle_controller.getPoseByLookAt(position, look_at)
-    pose.outputInfo()
-
-    agent_state = circle_controller.getAgentStateByAgentLookAt(position, look_at)
-    agent_state = circle_controller.getAgentStateFromAgentLookAt(look_at, move_direction)
-
-    input_key_list = INPUT_KEY_DICT.keys()
-    for input_key in input_key_list:
-        agent_state = circle_controller.getAgentStateByKey(input_key)
+    circle_controller.test()
     return True
 
