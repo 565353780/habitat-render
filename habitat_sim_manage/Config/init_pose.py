16a3b9f7fb4647418b45761154dcd65e6314084b
chLi <565353780@qq.com>
2022-10-04 20:31:23 +0800

    update init pose

--- habitat_sim_manage/Config/init_pose.py
+++ habitat_sim_manage/Config/init_pose.py
@@ -6,7 +6,7 @@ from habitat_sim_manage.Data.rad import Rad
 from habitat_sim_manage.Data.pose import Pose
 
 INIT_POSITION = Point(0.0, 1.5, 0.0)
-INIT_RAD = Rad(0.0, 0.0)
+INIT_RAD = Rad(0.0, 0.0, 0.0)
 INIT_POSE = Pose(INIT_POSITION, INIT_RAD)
 INIT_RADIUS = 2.0
 
